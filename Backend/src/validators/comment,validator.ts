783cc4a2c89bdc2b34d7044a232ffadd620edc16
shuayb mohammed <shuaybmohammed244@gmail.com>
2026-06-04 17:36:56 +0300

    feat: add validation schemas for comments and reactions

---
+++ Backend/src/validators/comment,validator.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+const reactionTypes = ["feel_this", "not_alone", "stay_strong", "sending_strength"] as const;
+
+export const createCommentSchema = z.object({
+  content:z.string().min(1,"Comment is required").max(2000,"Comment is too long").trim()
+})
+export const reactionSchema = z.object({
+  type: z.enum(reactionTypes, "Invalid reaction type")
+})
+export const commentQuerySchema = z.object({
+  page : z.coerce.number().min(1).optional().default(1),
+  limit : z.coerce.number().min(1).max(50).optional().default(10)
+})
+export type CreateCommentInput = z.infer<typeof createCommentSchema>;
+export type ReactionInput = z.infer<typeof reactionSchema>;
+export type CommentQueryInput = z.infer<typeof commentQuerySchema>;
